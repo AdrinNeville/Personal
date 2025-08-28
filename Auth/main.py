f6d2c38a808fff64dad45a5eb54e5f54c4ecc43e
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-08-28 10:54:52 +0530

    []

---
+++ Auth/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, timedelta
+import jwt
