498474a6295be746d38ce728c0ce926da7d787e8
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-08-29 14:59:45 +0530

    [main]

--- Auth/main.py
+++ Auth/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000"],  # or restrict to your frontend URL
+    allow_origins=["http://127.0.0.1:3000","http//:localhost:3000"],  # or restrict to your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
