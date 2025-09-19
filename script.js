9ea82099fd55ac9b7a8c24be4c2198a33cbbbecd
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-09-19 11:46:55 +0530

    deploy check

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ async function handleFormSubmit(e) {
     submitBtn.disabled = true;
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/contact", {
+        const response = await fetch("https://auth-fhzj.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
